994f2596c2e882511653285df0a98ce4ed0b663e
Abhay Maurya <127673236+AbhayGRT@users.noreply.github.com>
2023-09-16 16:42:40 +0530

    Add files via upload

---
+++ deploy-script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+git clone https://github.com/AbhayGRT/SimpleCICDPipeliine.git
+cd SimpleCICDPipeliine
+
+sudo docker build -t abhaygrt/instaloader .
+
+sudo docker run -d -p 5000:5000 abhaygrt/instaloader
+
